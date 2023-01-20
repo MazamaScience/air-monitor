3a2424510a77b3188f40e63f22a86704f5399544
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2023-01-19 16:10:42 -0800

    feat: added createGeoJSON()

---
+++ tests/interactive_tests.js
@@ -0,0 +1,17 @@
+import Monitor from "../index.js";
+
+const monitor = new Monitor();
+
+await monitor.loadLatest("airnow");
+
+console.log(monitor.count());
+
+let id = monitor.getIDs()[234];
+
+let daily = monitor.getDailyAverage(id);
+
+let diurnal = monitor.getDiurnalAverage(id);
+
+let geojson = monitor.createGeoJSON();
+
+let z = 1;
